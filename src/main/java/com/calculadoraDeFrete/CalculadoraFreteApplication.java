b41300ad467cc31777438aa71982b23041a1e1b8
Estefania Carrer <estefania.carrer@hotmail.com>
2023-10-18 12:31:40 -0300

    first commit

---
+++ src/main/java/com/calculadoraDeFrete/CalculadoraFreteApplication.java
@@ -0,0 +1,13 @@
+package com.calculadoraDeFrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraFreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraFreteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calculadoraDeFrete/CalculadoraFreteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculadoraDeFrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraFreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
