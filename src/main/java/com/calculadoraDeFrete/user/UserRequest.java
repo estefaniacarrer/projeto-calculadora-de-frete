9370490e009e239551be09b526077bd8833a3eef
IurydJR <iury.djr.ti@gmail.com>
2023-10-23 09:05:08 -0300

    feat(UserRequest): adição da classe

--- src/main/java/com/calculadoraDeFrete/user/UserRequest.java
+++ src/main/java/com/calculadoraDeFrete/user/UserRequest.java
@@ -1,4 +1,24 @@
 package com.calculadoraDeFrete.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class UserRequest {
+
+    @Email
+    private String userEmail;
+    @Pattern(regexp = "[\\w.]{5,20}", message = "Username deve ser alfanumérico entre 5 e 20 caracteres (lowercase, uppercase, numbers, _, .)")
+    private String userName;
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$",
+            message = "A senha deve conter de 8 a 20 caracteres (minúscula, maiúscula, números, caracteres especiais, não sequencial)")
+    private String userPassword;
+
+
+
 }
