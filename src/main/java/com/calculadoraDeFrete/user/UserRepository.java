8c808353563603af1547c5319d75b17542c3c01b
IurydJR <iury.djr.ti@gmail.com>
2023-10-23 09:41:41 -0300

    feat(UserRepository): adução de classe

--- src/main/java/com/calculadoraDeFrete/user/UserRepository.java
+++ src/main/java/com/calculadoraDeFrete/user/UserRepository.java
@@ -1,4 +1,20 @@
 package com.calculadoraDeFrete.user;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    //projection
+    @Query("""
+    select new com.calculadoraDeFrete.user.UserDto(u.email, u.username, u.senha)
+     from Usuario u where u.username = :username
+    """)
+    Optional<UserDto> findByUserName(@Param("userName") String userName);
+
 }
