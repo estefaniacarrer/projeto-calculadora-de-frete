3e86dbbc62b1d8fe5e975897c24b3ab1aaa5618d
IurydJR <iury.djr.ti@gmail.com>
2023-10-23 09:00:30 -0300

    feat(UserDto): adição da classe

--- src/main/java/com/calculadoraDeFrete/user/UserDto.java
+++ src/main/java/com/calculadoraDeFrete/user/UserDto.java
@@ -1,4 +1,20 @@
 package com.calculadoraDeFrete.user;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class UserDto {
+
+    private String email;
+    private String username;
+    private String password;
+
+    public UserDto(String username) {
+        this.username = username;
+    }
 }
+
--- src/main/java/com/calculadoraDeFrete/user/userResponse.java
+++ src/main/java/com/calculadoraDeFrete/user/userResponse.java
@@ -1,4 +1,4 @@
 package com.calculadoraDeFrete.user;
 
-public class UserResponse {
+public class userResponse {
 }
